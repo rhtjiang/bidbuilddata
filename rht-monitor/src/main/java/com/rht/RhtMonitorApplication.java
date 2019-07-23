57176890a9ea41029310bc6d65ed23b17c9517cb
rhtjiang <1402523316@qq.com>
2019-07-23 17:39:23 +0800

    启动器修改

--- rht-monitor/src/main/java/com/rht/RhtMonitorApplication.java
+++ rht-monitor/src/main/java/com/rht/RhtMonitorApplication.java
@@ -1,8 +1,15 @@
 package com.rht;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * 启动器
+ */
+@EnableAdminServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class RhtMonitorApplication {
 
