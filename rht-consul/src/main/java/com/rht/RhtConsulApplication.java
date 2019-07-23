ad663fac3349097b6223a69825423a3eff13d079
rhtjiang <1402523316@qq.com>
2019-07-23 15:20:26 +0800

    新增微服务架构，注册中心模块

---
+++ rht-consul/src/main/java/com/rht/RhtConsulApplication.java
@@ -0,0 +1,13 @@
+package com.rht;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhtConsulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RhtConsulApplication.class, args);
+    }
+
+}
