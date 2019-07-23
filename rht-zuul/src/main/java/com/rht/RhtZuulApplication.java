413276e16c86d532a32ba1563812ed5f949b9788
rhtjiang <1402523316@qq.com>
2019-07-23 15:27:04 +0800

    新增微服务架构，网关模块

---
+++ rht-zuul/src/main/java/com/rht/RhtZuulApplication.java
@@ -0,0 +1,13 @@
+package com.rht;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhtZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RhtZuulApplication.class, args);
+    }
+
+}
